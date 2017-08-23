4e6f47691550c9f3f802f313ddde3295ed849534
ptahinduka <remidnetworks@gmail.com>
2017-08-23 10:32:21 -0700

    The Python file that runs the Flask front end

---
+++ app.py
@@ -0,0 +1,28 @@
+### @petertahinduka ######
+
+### This file fires up the ShopEasy Application ###
+
+#from flask_login import LoginManager
+#from user import User
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('shopeasy_index.html')
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signin.html')
+
+
+@app.route('/showHome')
+def showHome():
+    return render_template('home.html')
+
+
+        
+if __name__ == "__main__":
+    app.run(port=5002)
