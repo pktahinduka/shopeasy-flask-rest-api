0114583d5f17eecf0cd795c9d8b0fcd60dfa7300
ptahinduka <remidnetworks@gmail.com>
2017-08-23 17:21:03 -0700

    Some changes in the test configuration files

--- project/server/config.py
+++ project/server/config.py
@@ -2,7 +2,7 @@
 
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-postgres_local_base = 'postgresql://postgres:peter926@localhost/'
+postgres_local_base = 'postgresql://postgres:@localhost/'
 database_name = 'shopeasyAuth'
 
 
